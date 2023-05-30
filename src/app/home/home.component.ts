be06b0beae0834da1a7fa69d53a5196b60c4c9a1
felipebhz <fbhz85@gmail.com>
2023-05-30 16:25:30 -0300

    1 file modified, 3 added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
-
+import { HousingService } from '../housing.service';
 // Interfaces | Types
 import { HousingLocation } from '../housinglocation';
 
@@ -21,119 +21,13 @@ import { HousingLocation } from '../housinglocation';
   [housingLocation]="housingLocation"></app-housing-location>
   </section>
   `,
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  housingLocationList: HousingLocation[] = [
-    {
-      id: 0,
-      name: 'Acme Fresh Start Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo:
-        'https://cdn.jsdelivr.net/gh/felipebhz/angular-tutorial-first-app@main/src/assets/house-1.jpg',
-      availableUnits: 4,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 1,
-      name: 'A113 Transitional Housing',
-      city: 'Santa Monica',
-      state: 'CA',
-      photo:
-        'https://cdn.jsdelivr.net/gh/felipebhz/angular-tutorial-first-app@main/src/assets/house-2.jpg',
-      availableUnits: 0,
-      wifi: false,
-      laundry: true,
-    },
-    {
-      id: 2,
-      name: 'Warm Beds Housing Support',
-      city: 'Juneau',
-      state: 'AK',
-      photo:
-        'https://cdn.jsdelivr.net/gh/felipebhz/angular-tutorial-first-app@main/src/assets/house-3.jpg',
-      availableUnits: 1,
-      wifi: false,
-      laundry: false,
-    },
-    {
-      id: 3,
-      name: 'Homesteady Housing',
-      city: 'Chicago',
-      state: 'IL',
-      photo:
-        'https://cdn.jsdelivr.net/gh/felipebhz/angular-tutorial-first-app@main/src/assets/house-4.jpg',
-      availableUnits: 1,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 4,
-      name: 'Happy Homes Group',
-      city: 'Gary',
-      state: 'IN',
-      photo:
-        'https://cdn.jsdelivr.net/gh/felipebhz/angular-tutorial-first-app@main/src/assets/house-5.jpg',
-      availableUnits: 1,
-      wifi: true,
-      laundry: false,
-    },
-    {
-      id: 5,
-      name: 'Hopeful Apartment Group',
-      city: 'Oakland',
-      state: 'CA',
-      photo:
-        'https://cdn.jsdelivr.net/gh/felipebhz/angular-tutorial-first-app@main/src/assets/house-6.jpg',
-      availableUnits: 2,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 6,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo:
-        'https://cdn.jsdelivr.net/gh/felipebhz/angular-tutorial-first-app@main/src/assets/house-7.jpg',
-      availableUnits: 5,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 7,
-      name: 'Hopeful Housing Solutions',
-      city: 'Oakland',
-      state: 'CA',
-      photo:
-        'https://cdn.jsdelivr.net/gh/felipebhz/angular-tutorial-first-app@main/src/assets/house-8.jpg',
-      availableUnits: 2,
-      wifi: true,
-      laundry: true,
-    },
-    {
-      id: 8,
-      name: 'Seriously Safe Towns',
-      city: 'Oakland',
-      state: 'CA',
-      photo:
-        'https://cdn.jsdelivr.net/gh/felipebhz/angular-tutorial-first-app@main/src/assets/house-9.jpg',
-      availableUnits: 10,
-      wifi: false,
-      laundry: false,
-    },
-    {
-      id: 9,
-      name: 'Capital Safe Towns',
-      city: 'Portland',
-      state: 'OR',
-      photo:
-        'https://cdn.jsdelivr.net/gh/felipebhz/angular-tutorial-first-app@main/src/assets/house-1.jpg',
-      availableUnits: 6,
-      wifi: true,
-      laundry: true,
-    },
-  ];
+  housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
